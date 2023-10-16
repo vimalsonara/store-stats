a71024cfbda4f9b3197b55d02f2d14a9192a936a
vimalsonara <vvsonara95@gmail.com>
2023-10-16 09:09:56 +0530

    Add purchase create API

---
+++ app/api/purchase/route.ts
@@ -0,0 +1,40 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+import connectDB from "@/lib/db";
+import PurchaseEntry from "@/models/purchase";
+
+connectDB();
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    try {
+      const reqBody = await req.json();
+
+      const { date, userId, vendorId, items } = reqBody;
+
+      if (!date || !userId || !vendorId || !items) {
+        return NextResponse.json(
+          { error: "Any details can't be empty." },
+          { status: 400 }
+        );
+      }
+
+      const newPurchaseEntry = await PurchaseEntry.create({
+        date,
+        userId,
+        vendorId,
+        items,
+      });
+      return NextResponse.json({ newPurchaseEntry }, { status: 201 });
+    } catch (error: any) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+  } else {
+    return NextResponse.json(
+      { error: "You are not authenticated." },
+      { status: 401 }
+    );
+  }
+}
---
+++ models/purchase.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+interface Item {
+  itemName: string;
+  quantity: number;
+  price: number;
+}
+
+const purchaseModel = new mongoose.Schema(
+  {
+    date: {
+      type: Date,
+      default: Date.now,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+    vendorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendors",
+      required: true,
+    },
+    items: {
+      type: [
+        {
+          itemName: String,
+          quantity: Number,
+          price: Number,
+        },
+      ],
+      validate: [
+        {
+          validator: function (items: Item[]) {
+            return items.length > 0;
+          },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+const PurchaseEntry =
+  mongoose.models.PurchaseEntry ||
+  mongoose.model("PurchaseEntry", purchaseModel);
+
+export default PurchaseEntry;
