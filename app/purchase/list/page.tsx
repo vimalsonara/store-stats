51fb6814bac94ccb55060b67820149bdbb868a5c
vimalsonara <vvsonara95@gmail.com>
2023-10-22 19:32:12 +0530

    API integration purchase list

---
+++ app/purchase/list/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import DataTable from "../../components/dataTable";
+import { columns } from "./columns";
+import Link from "next/link";
+
+export type Purchase = {
+  _id: string;
+  date: string;
+  totalAmount: number;
+};
+
+export default function PurchaseList() {
+  const [purchaseList, setPurchaseList] = useState<Purchase[]>([]);
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session?.user.id) {
+      const getAllPurchase = async () => {
+        try {
+          const purchases = await axios.post("/api/purchase/list", {
+            userId: session?.user.id,
+          });
+          setPurchaseList(purchases.data);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      getAllPurchase();
+    }
+  }, [session]);
+  console.log(purchaseList);
+  return (
+    <div>
+      <div>
+        <ul className="flex gap-2 justify-around p-2">
+          <li>
+            <Link href={"/vendor/create"}>New Purchase</Link>
+          </li>
+        </ul>
+      </div>
+      <DataTable columns={columns} data={purchaseList} />
+    </div>
+  );
+}
