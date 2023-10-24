3645e54eb70e68b7c3bd110ccfe16d40ffa1190a
vimalsonara <vvsonara95@gmail.com>
2023-10-24 09:15:22 +0530

    Create Dashboard
    Display statistics of overall app

---
+++ app/components/dashboardCard.tsx
@@ -0,0 +1,16 @@
+interface DashboardCardProps {
+  cardTitle: string;
+  total: number;
+}
+
+export default function DashboardCard({
+  cardTitle,
+  total,
+}: DashboardCardProps) {
+  return (
+    <div className="border p-2 text-center rounded-lg ">
+      <div className="border-b p-2">{cardTitle}</div>
+      <div className="p-2">{total}</div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,58 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import DashboardCard from "@/app/components/dashboardCard";
+
+interface Purchase {
+  totalAmount: number;
+}
+
 export default function Home() {
   const { data: session } = useSession();
-  return <div>Dashboard</div>;
+  const [vendorList, setVendorList] = useState([]);
+  const [productList, setProductList] = useState([]);
+  const [purchaseList, setPurchaseList] = useState<Purchase[]>([]);
+
+  useEffect(() => {
+    if (session?.user.id) {
+      const getVendorProductPurchase = async () => {
+        try {
+          const vendors = await axios.post("/api/vendor/list", {
+            userId: session?.user.id,
+          });
+          const products = await axios.post("/api/product/list", {
+            userId: session?.user.id,
+          });
+          const purchases = await axios.post("/api/purchase/list", {
+            userId: session?.user.id,
+          });
+          setVendorList(vendors.data);
+          setProductList(products.data);
+          setPurchaseList(purchases.data);
+        } catch (error: any) {
+          console.log(error.message);
+        }
+      };
+      getVendorProductPurchase();
+    }
+  }, [session]);
+
+  const totalPurchase = purchaseList?.reduce(
+    (acc, purchase) => acc + purchase.totalAmount,
+    0
+  );
+
+  return (
+    <div className="flex gap-3 mt-2 flex-wrap">
+      <DashboardCard cardTitle={"Total Vendors"} total={vendorList.length} />
+      <DashboardCard cardTitle={"Total Products"} total={productList.length} />
+      <DashboardCard cardTitle={"Total Purchase"} total={purchaseList.length} />
+      <DashboardCard
+        cardTitle={"Total Purchase Amount"}
+        total={totalPurchase}
+      />
+    </div>
+  );
 }
