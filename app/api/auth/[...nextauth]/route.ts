958fa79d878ed8e8b1c646ba529180185d12a5c4
vimalsonara <vvsonara95@gmail.com>
2023-10-29 21:53:31 +0530

    Added firebase due to appwrite issue with nextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,22 @@ import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import connectDB from "@/lib/db";
 import User from "@/models/user";
+import { db } from "@/lib/firebaseConfig";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  serverTimestamp,
+  where,
+} from "firebase/firestore";
+
+interface Users {
+  id: string;
+  email: string;
+}
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -13,24 +29,52 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     async session({ session }) {
-      if (session && session.user) {
-        const sessionUser = await User.findOne({ email: session.user.email });
-        if (sessionUser) {
-          session.user.id = sessionUser._id.toString();
-        }
-      }
+      const userRef = collection(db, "users");
+      let users: Users[] = [];
+      getDocs(userRef)
+        .then((snapshot) => {
+          snapshot.docs.forEach((doc) => {
+            const userData = doc.data();
+            const user: Users = {
+              id: doc.id,
+              email: userData.email,
+            };
+            users.push(user);
+          });
+
+          console.log(users, "users");
+
+          const sessionUser = users.find(
+            (user) => user.email === session.user.email
+          );
+
+          if (sessionUser) {
+            session.user.id = sessionUser.id.toString();
+          }
+        })
+        .catch((error: any) => {
+          console.log(error.message);
+        });
+
       return session;
     },
     async signIn({ profile }) {
+      const userRef = collection(db, "users");
+      const emailCondition = where("email", "==", profile?.email);
+      const q = query(userRef, emailCondition);
+
       try {
-        await connectDB();
-        const userExist = await User.findOne({ email: profile?.email });
-        if (!userExist) {
-          const user = await User.create({
+        const querySnapshot = await getDocs(q);
+
+        if (querySnapshot.empty) {
+          const createdAt = serverTimestamp();
+          const newUserDoc = await addDoc(userRef, {
             email: profile?.email,
             name: profile?.name,
+            createdAt,
           });
         }
+
         return true;
       } catch (error) {
         console.log("error", error);
