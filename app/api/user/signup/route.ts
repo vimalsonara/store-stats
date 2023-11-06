fa718df1cf70e5766262441432a8a4e73aad1577
vimalsonara <vvsonara95@gmail.com>
2023-11-06 23:04:02 +0530

    WIP User signup route

--- app/api/user/signup/route.ts
+++ app/api/user/signup/route.ts
@@ -1,6 +1,53 @@
+import { db } from "@/lib/firebaseConfig";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  serverTimestamp,
+  where,
+} from "firebase/firestore";
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+interface Users {
+  id: string;
+  name: string;
+  email: string;
+}
 
 export async function POST(req: NextRequest) {
-  const reqBody = await req.json();
-  console.log(reqBody);
+  try {
+    const reqBody = await req.json();
+    const { name, email, password } = reqBody;
+
+    const userRef = collection(db, "users");
+    const emailCondition = where("email", "==", email);
+    const q = query(userRef, emailCondition);
+    if (!email || !password || !name) {
+      return NextResponse.json("Any details can't be empty", { status: 400 });
+    }
+
+    const querySnapshot = await getDocs(q);
+
+    if (querySnapshot.empty) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const createdAt = serverTimestamp();
+      const newUser = await addDoc(userRef, {
+        name,
+        email,
+        password: hashedPassword,
+        createdAt,
+      });
+      console.log(hashedPassword);
+      return NextResponse.json(newUser);
+    } else {
+      return NextResponse.json(
+        { error: "User already exist" },
+        { status: 403 }
+      );
+    }
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
