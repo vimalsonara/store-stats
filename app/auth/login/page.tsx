e599147430bfd4d59277847df8936c23eef07627
vimalsonara <vvsonara95@gmail.com>
2023-11-11 20:55:05 +0530

    Login page style correcton

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -23,7 +23,7 @@ export default function Login() {
   };
 
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center">
+    <div className="flex min-h-screen w-screen flex-col items-center justify-center">
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col gap-2 border p-5 rounded-lg"
