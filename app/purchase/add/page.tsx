84dd2e741a8ba9997e5773b1782425b5c9fece5d
vimalsonara <vvsonara95@gmail.com>
2023-11-30 22:17:38 +0530

    WIP purchase form re-style

--- app/purchase/add/page.tsx
+++ app/purchase/add/page.tsx
@@ -6,6 +6,9 @@ import { useForm, SubmitHandler, useFieldArray } from "react-hook-form";
 import toast, { Toaster } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@radix-ui/react-dropdown-menu";
+import { Input } from "@/components/ui/input";
 
 interface Item {
   itemName: string;
@@ -122,104 +125,113 @@ export default function PurchaseEntry() {
   };
 
   return (
-    <div>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-2 border p-5 rounded-lg"
-      >
-        <input
-          type="date"
-          placeholder="Purchase Date"
-          className={
-            errors.date
-              ? "border-2 border-red-500 rounded p-1 outline-none"
-              : "rounded p-1 outline-none "
-          }
-          {...register("date", { required: true })}
-        />
-        {errors.date && <span className="text-red-500">Date required</span>}
-        <select
-          {...register("vendorId", { required: true })}
-          className={
-            errors.vendorId
-              ? "border-2 border-red-500 rounded p-1 outline-none"
-              : "rounded p-1 outline-none "
-          }
-          onChange={(e) => {
-            const selectedVendorId = e.target.value;
-            const selectedVendor = vendorList.find(
-              (vendor) => vendor.id === selectedVendorId
-            );
-            setSelectedVendorName(selectedVendor?.vendorName || "");
-          }}
-        >
-          <option value="">Select</option>
-          {vendorList.map((vendor) => (
-            <option key={vendor.id} value={vendor.id}>
-              {vendor.vendorName}
-            </option>
-          ))}
-        </select>
-        {errors.vendorId && (
-          <span className="text-red-500">Vendor required</span>
-        )}
-
-        {fields.map((item, index) => (
-          <div key={item.id} className="flex gap-2 flex-wrap">
+    <div className="">
+      <Card className="max-w-3xl mx-auto my-auto p-3 space-y-6">
+        <CardHeader>
+          <CardTitle className={"text-center"}>New Purchase</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div className="space-y-2">
+              <Label>Date</Label>
+              <Input
+                type="date"
+                placeholder="Purchase Date"
+                className={
+                  errors.date
+                    ? "border-2 border-red-500 rounded p-1 outline-none"
+                    : "rounded p-1 outline-none "
+                }
+                {...register("date", { required: true })}
+              />
+              {errors.date && (
+                <span className="text-red-500">Date required</span>
+              )}
+            </div>
             <select
-              {...register(`items.${index}.itemName`)}
-              className={"rounded p-1 outline-none "}
+              {...register("vendorId", { required: true })}
+              className={
+                errors.vendorId
+                  ? "border-2 border-red-500 rounded p-1 outline-none"
+                  : "rounded p-1 outline-none "
+              }
+              onChange={(e) => {
+                const selectedVendorId = e.target.value;
+                const selectedVendor = vendorList.find(
+                  (vendor) => vendor.id === selectedVendorId
+                );
+                setSelectedVendorName(selectedVendor?.vendorName || "");
+              }}
             >
               <option value="">Select</option>
-              {productList.map((product) => (
-                <option key={product.id} value={product.product}>
-                  {product.product}
+              {vendorList.map((vendor) => (
+                <option key={vendor.id} value={vendor.id}>
+                  {vendor.vendorName}
                 </option>
               ))}
             </select>
-            <input
-              type="number"
-              placeholder="Quantity"
-              step="1"
-              min="1"
-              className={"rounded p-1 outline-none "}
-              {...register(`items.${index}.quantity`, { required: true })}
-              onChange={(e) =>
-                handleQuantityChange(index, parseInt(e.target.value))
-              }
-            />
-            <input
-              type="number"
-              step="1"
-              min="1"
-              placeholder="Price"
-              className={"rounded p-1 outline-none"}
-              {...register(`items.${index}.price`, { required: true })}
-              onChange={(e) =>
-                handlePriceChange(index, parseInt(e.target.value))
-              }
-            />
-            {index > 0 && (
-              <button
-                className="bg-red-500  p-1 rounded-md"
-                onClick={() => remove(index)}
-              >
-                Remove
-              </button>
+            {errors.vendorId && (
+              <span className="text-red-500">Vendor required</span>
             )}
-          </div>
-        ))}
-        <div className="flex justify-center items-center gap-2">
-          {totalAmount.toString()}
-          <button
-            className="bg-blue-500 p-1 rounded-md"
-            onClick={() => append({ itemName: "", quantity: 0, price: 0 })}
-          >
-            Add Item
-          </button>
-          <input className="bg-green-500 p-1 rounded-md" type="submit" />
-        </div>
-      </form>
+
+            {fields.map((item, index) => (
+              <div key={item.id} className="flex gap-2 flex-wrap">
+                <select
+                  {...register(`items.${index}.itemName`)}
+                  className={"rounded p-1 outline-none "}
+                >
+                  <option value="">Select</option>
+                  {productList.map((product) => (
+                    <option key={product.id} value={product.product}>
+                      {product.product}
+                    </option>
+                  ))}
+                </select>
+                <input
+                  type="number"
+                  placeholder="Quantity"
+                  step="1"
+                  min="1"
+                  className={"rounded p-1 outline-none "}
+                  {...register(`items.${index}.quantity`, { required: true })}
+                  onChange={(e) =>
+                    handleQuantityChange(index, parseInt(e.target.value))
+                  }
+                />
+                <input
+                  type="number"
+                  step="1"
+                  min="1"
+                  placeholder="Price"
+                  className={"rounded p-1 outline-none"}
+                  {...register(`items.${index}.price`, { required: true })}
+                  onChange={(e) =>
+                    handlePriceChange(index, parseInt(e.target.value))
+                  }
+                />
+                {index > 0 && (
+                  <button
+                    className="bg-red-500  p-1 rounded-md"
+                    onClick={() => remove(index)}
+                  >
+                    Remove
+                  </button>
+                )}
+              </div>
+            ))}
+            <div className="flex justify-center items-center gap-2">
+              {totalAmount.toString()}
+              <button
+                className="bg-blue-500 p-1 rounded-md"
+                onClick={() => append({ itemName: "", quantity: 0, price: 0 })}
+              >
+                Add Item
+              </button>
+              <input className="bg-green-500 p-1 rounded-md" type="submit" />
+            </div>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
